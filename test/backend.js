97dea6f104b07ebe291cea00ecc79682b498becb
Adnane SAGHIR <adnane.saghir@thomsonreuters.com>
2016-11-11 21:00:41 +0100

    ES default search size
    added test to bypass ES default search size of 10 results

--- test/backend.js
+++ test/backend.js
@@ -3,6 +3,7 @@
 var Backend = require('../lib');
 var assert = require('chai').assert;
 var elasticsearch = require('elasticsearch');
+var Acl = require('acl');
 var client = new elasticsearch.Client({
   apiVersion: '1.7',
   hosts: ['localhost:9200'],
@@ -38,15 +39,41 @@ describe('Backend Tests', function() {
   it('should handle custom parameters', function() {
     custom = new Backend({
       client: client,
-      index: 'foo',
-      prefix: 'bar',
+      index: 'acltest',
+      prefix: 'bar_',
       refresh: true,
       size: 5
     });
     assert.instanceOf(custom, Backend);
-    assert.strictEqual(custom.index, 'foo');
-    assert.strictEqual(custom.prefix, 'bar');
+    assert.strictEqual(custom.index, 'acltest');
+    assert.strictEqual(custom.prefix, 'bar_');
     assert.strictEqual(custom.refresh, true);
     assert.strictEqual(custom.size, 5);
+  });
+  it('should bypass ES default search size of 10 results', function(done) {
+    var acl = new Acl(custom);
+    custom.clean(function(err, res) {
+      if (err) {
+        console.error(err);
+      }
+      var roles = [];
+      for (var i = 1; i <= 15; i++) {
+        roles.push('role_' + i);
+      }
+      acl.addUserRoles('U1', roles, function(err) {
+        if (err) {
+          done(err)
+        } else {
+          acl.userRoles('U1', function(err, result) {
+            if (err) {
+              done(err);
+            } else {
+              assert.isOk(result.length >= 15);
+              done();
+            }
+          });
+        }
+      });
+    });
   });
 });
